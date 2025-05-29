c7eb4601fbcb72f0e3ac5ef5b5639e43403a6069
JiggyStardust <saaraleni@gmail.com>
2025-05-29 12:16:49 +0300

    main comments

--- src/main.cpp
+++ src/main.cpp
@@ -18,16 +18,3 @@ int	main(int argc, char **argv)
     return (0);
 }
 
-// int	main(int argc, char **argv)
-// {
-// 	// (void)argc;
-// 	// (void)argv;
-// 	if (argc != 2 || std::string(argv[1]) != "configuration/webserv.conf")
-// 	{
-// 		std::cout << "\nRun with \"./webserv configuration/webserv.conf\"" << std::endl;
-// 		return (1);
-// 	}
-// 	configParser(argv[1]);
-// 	// mvp();
-// 	return (0);
-// }
