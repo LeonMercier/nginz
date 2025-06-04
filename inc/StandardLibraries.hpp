bb62de842c88da9b43b66b64190475599f6a837c
JiggyStardust <saaraleni@gmail.com>
2025-06-04 15:38:38 +0300

    Squashed. Has images and uploads directories inside www and autonindexing implemented.
    Directorys weren't pushed to git without some example files in them
    
    before rebasing

--- inc/StandardLibraries.hpp
+++ inc/StandardLibraries.hpp
@@ -1,20 +1,21 @@
 #pragma once
 
 
-#include <exception>
 #include <algorithm>
+#include <asm-generic/socket.h>
 #include <cstddef> // for size_t?
+#include <exception>
+#include <fcntl.h> // fcntl() // only for MacOS?
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <netinet/in.h>
 #include <sstream> // for istrinstream
+#include <stdexcept>
 #include <string>
 //#include <strings.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
 #include <unistd.h> // sleep()
 #include <vector>
-#include <asm-generic/socket.h>
-#include <stdexcept>
-#include <fcntl.h> // fcntl()
