30a344d75cab3226aaec5f378ec7e7033814746c
lemercie <lemercie@student.hive.fi>
2025-07-09 13:51:53 +0300

    add bad CGI scripts for testing

---
+++ www/cgi/crash.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+##############################################################################
+# Maybe technically this is an unhandled exception, not a crash but whatever
+##############################################################################
+
+import os
+
+raw_query = os.environ['YOLO']
